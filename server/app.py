824ad04c9d30692026e270ced03eb1776c1ec66d
kelvin <kelvinmutugi336@gmil.com>
2024-07-04 22:27:18 +0300

    done

--- server/app.py
+++ server/app.py
@@ -19,22 +19,26 @@ db.init_app(app)
 api = Api(app)
 
 class ClearSession(Resource):
-
     def delete(self):
     
         session['page_views'] = None
         session['user_id'] = None
 
         return {}, 204
+api.add_resource(ClearSession, '/clear')
 
-class IndexArticle(Resource):
-    
+
+
+class IndexArticle(Resource): 
     def get(self):
         articles = [article.to_dict() for article in Article.query.all()]
+
         return articles, 200
+api.add_resource(IndexArticle, '/articles')
+
+
 
 class ShowArticle(Resource):
-
     def get(self, id):
         session['page_views'] = 0 if not session.get('page_views') else session.get('page_views')
         session['page_views'] += 1
@@ -47,10 +51,36 @@ class ShowArticle(Resource):
             return make_response(article_json, 200)
 
         return {'message': 'Maximum pageview limit reached'}, 401
-
-api.add_resource(ClearSession, '/clear')
-api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
+
+
+
+class Login(Resource):
+    def post(self):
+        user = User.query.filter(User.username == request.get_json()['username']).first()
+        session['user_id'] = user.id
+        return user.to_dict(), 200   
+api.add_resource(Login, '/login')
+
+
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
+api.add_resource(Logout, '/logout')
+
+
+
+class CheckSession(Resource):
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        
+        if user:
+            return user.to_dict(), 200
+        else:
+            return {}, 401
+api.add_resource(CheckSession, '/check_session')        
 
 
 if __name__ == '__main__':
